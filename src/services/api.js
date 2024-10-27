b08fdc0553ca95cb626c29a278158e2ae9b00163
Ziyodullo <ziyodillo808@gmail.com>
2024-10-27 08:11:16 +0300

    services edited

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,3 @@
-// import axios from 'axios';
-
-// // Base URL
-// const API_URL = 'https://ferganamedia.pythonanywhere.com/';
-
-
-// const api = axios.create({
-//     baseURL: API_URL,
-// });
-
-// export default api;
-
 import axios from 'axios';
 
 // Base URL
